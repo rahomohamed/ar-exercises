0a6476b21eafc7d23324be46138424a2202d7247
rahomohamed <rahojmohamed@gmail.com>
2020-04-02 02:14:21 +0000

    completed ex 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,10 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter a store name"
+store_name = gets.chomp
+store = Store.create(name: store_name)
+puts store.errors[:name]
+puts store.errors[:annual_revenue]
+puts store.errors[:mens_apparel]
+puts store.errors[:womens_apparel]
